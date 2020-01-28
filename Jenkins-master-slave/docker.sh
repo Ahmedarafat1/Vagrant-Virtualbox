c9a176fdb0f416551ee166095a389e42d4de5c2f
root <root@jenkins-master>
2020-01-28 07:23:54 +0000

    jenkins mster slave

---
+++ Jenkins-master-slave/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## install docker
+set -x
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker vagrant
+
+## install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
