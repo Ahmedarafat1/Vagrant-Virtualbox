5f3491a401bf220fb2a213637e76cfb2a491afd8
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2020-02-04 20:31:46 +0100

    Update docker.sh

--- Jenkins-master-slave/docker.sh
+++ Jenkins-master-slave/docker.sh
@@ -12,3 +12,9 @@ sudo usermod -aG docker vagrant
 ## install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir jenkins-master
+chmod -R 777 jenkins-master
+
+cd /vagrant
+docker-compose up -d 
